a4905c5ae025abebe8e25aecb70ed0e5d3e7b775
Juliana Sartori <juliana.sartori@rethink.dev>
2023-11-16 18:35:10 -0300

    algumas alteracoes

--- sistema-gta/src/App.js
+++ sistema-gta/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Listagem from "./components/listagem/Listagem.js";
 import Cadastro from "./components/cadastro/cadastro.js";
@@ -6,18 +5,15 @@ import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
-    // <div className="App" style={styles}>
     <div className="App">
       <BrowserRouter>
         <div>
           <Routes>
-            <Route path='/' element={<Cadastro />}></Route>
-            <Route path='/listagem' element={<Listagem />}></Route>
+            <Route path="/" element={<Cadastro />}></Route>
+            <Route path="/listagem" element={<Listagem />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
-
-
     </div>
   );
 }
--- sistema-gta/src/components/cadastro/name/name.js
+++ sistema-gta/src/components/cadastro/name/name.js
@@ -2,6 +2,7 @@ import "./name.css";
 
 const Name = (props) => {
   const { title } = props;
+
   return (
     <div className="dado">
       <h3>{title}</h3>
