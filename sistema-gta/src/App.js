ea2edc501b3cb743efb5c0ad2188f11e79795093
Juliana Sartori <juliana.sartori@rethink.dev>
2023-11-16 22:14:34 -0300

    melhorias

--- sistema-gta/src/App.js
+++ sistema-gta/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Listagem from "./components/listagem/Listagem.js";
 import Cadastro from "./components/cadastro/cadastro.js";
@@ -6,21 +5,18 @@ import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import { Trabalhos } from "./data.js";
 
 function App() {
-  window.localStorage.setItem('trabalhos', JSON.stringify(Trabalhos))
+  window.localStorage.setItem("trabalhos", JSON.stringify(Trabalhos));
 
   return (
-    // <div className="App" style={styles}>
     <div className="App">
       <BrowserRouter>
         <div>
           <Routes>
-            <Route path='/' element={<Cadastro />}></Route>
-            <Route path='/listagem' element={<Listagem />}></Route>
+            <Route path="/" element={<Cadastro />}></Route>
+            <Route path="/listagem" element={<Listagem />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
-
-
     </div>
   );
 }
